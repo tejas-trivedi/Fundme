3d403c620386fea4b113e46df2c5dac7b65ba22a
tejas-trivedi <57986361+tejas-trivedi@users.noreply.github.com>
2019-11-23 10:56:40 +0530

    setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ListComponent } from './list/list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule, MatButtonModule,
+  MatSidenavModule, MatIconModule, MatListModule, MatCardModule,
+  MatTabsModule, MatInputModule, MatProgressSpinnerModule } from '@angular/material';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,MatToolbarModule, MatButtonModule,
+    MatSidenavModule, MatIconModule, MatListModule, MatCardModule,
+    MatTabsModule, MatInputModule, MatProgressSpinnerModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
